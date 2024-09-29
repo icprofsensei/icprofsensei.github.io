602ed9f4fe6fa31be9c30c57391dc2641500d1ef
Dhylan Patel <dhylan@live.co.uk>
2024-09-29 15:42:25 +0100

    slightchange

--- wdemo/settings.py
+++ wdemo/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'wdemo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'polls/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
