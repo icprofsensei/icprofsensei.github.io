bde6a37a37d5fdc3eecf9131ec362943e1180e42
Dhylan Patel <dhylan@live.co.uk>
2024-10-06 14:16:23 +0100

    login

--- wdemo/urls.py
+++ wdemo/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from register import views as v
+from . import views as homeviews
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("polls/", include("polls.urls")),
     path("register/", v.register, name = "register"),
+    path('', homeviews.home, name = "home"),
+    path('', include("django.contrib.auth.urls")),
 ]
