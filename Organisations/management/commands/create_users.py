2d98b45d55128748606ce694e2226b85448b7a2c
Dhylan Patel <139132349+icprofsensei@users.noreply.github.com>
2024-10-14 12:47:46 +0000

    command

---
+++ Organisations/management/commands/create_users.py
@@ -0,0 +1,16 @@
+# your_app/management/commands/create_users_profile.py
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from Organisations.models import UserProfile  # Make sure to import your UserProfile model
+
+class Command(BaseCommand):
+    help = 'Get or create UserProfiles for all users'
+
+    def handle(self, *args, **kwargs):
+        users = User.objects.all()
+        for user in users:
+            profile, created = UserProfile.objects.get_or_create(user=user)
+            if created:
+                self.stdout.write(self.style.SUCCESS(f'UserProfile for {user.username} created successfully.'))
+            else:
+                self.stdout.write(self.style.WARNING(f'UserProfile for {user.username} already exists.'))
