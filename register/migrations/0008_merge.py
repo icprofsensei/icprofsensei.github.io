2d36b62a902a672613f8b223b6299bb428a1fcd8
Dhylan Patel <dhylan@live.co.uk>
2024-10-21 23:29:40 +0100

    ups

---
+++ register/migrations/0008_merge.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('register', '0002_userprofile_date_of_birth'),
+        ('register', '0007_alter_userprofile_ethnicity'),
+    ]
+
+    operations = [
+        # Add operations here if needed
+    ]
